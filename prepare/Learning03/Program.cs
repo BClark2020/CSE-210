f23ff978d5c03566e690c61ff3ebccb6d39be133
BClark21 <brenden.t.clark2020@gmail.com>
2024-05-30 15:12:17 -0600

    updated: made it so keys control different functions, backspace will reveal words, enter will remove words, and space will stop removing words

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,84 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
+
+
+
+class Fraction
+{
+
+    private int _numerator;
+    private int _denominator;
+    public void set_bottom(int Denominator = 1)
+    {
+        _denominator = Denominator;
+    }
+    public void set_top(int Numerator = 1)
+    {
+        _numerator = Numerator;
+    }
+}
+
 
 class Program
 {
+
+    public int get_top()
+    {
+        int _BCNumerator = '0';
+        bool _BCBool_var = true;
+
+        do
+        {
+            try
+            {
+                Console.Write("Numerator: ");
+                _BCNumerator = int.Parse(Console.ReadLine());
+                _BCBool_var = true;
+                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("PLease enter an integer");
+                _BCBool_var = false;
+            }
+
+        } while (_BCBool_var == false);
+
+        return _BCNumerator;
+    }  
+    public int get_bottom()
+    {
+        int _BCDenominator = '1';
+        bool _BCBool_var = true;
+
+        do
+        {
+            try
+            {
+                Console.Write("Denominator: ");
+                _BCDenominator = int.Parse(Console.ReadLine());
+                _BCBool_var = true;
+                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("PLease enter an integer");
+                _BCBool_var = false;
+            }
+
+        } while (_BCBool_var == false);
+
+        return _BCDenominator;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac = new Fraction();
+        frac.set_bottom();
+        frac.set_top();
+        
+        Console.WriteLine($"{}");
+
+
+
     }
 }
