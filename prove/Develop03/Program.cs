09d3030be68359e348cd07a036b927f71c7deccd
BClark21 <brenden.t.clark2020@gmail.com>
2024-05-30 13:06:55 -0600

    Updated- added, a user controled word drop rate, a reveal command, an end command, the option to memorize another scripture without leaving the program, and separated the program class into two new functions, one to run the word removal code, and the other to get a user input to see if they want to repeat the program again. I also added a new function in  the word class that displays the revealed full scripture.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -14,12 +14,14 @@ using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Data;
 using System.IO;
+using System.Linq.Expressions;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
 using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic.FileIO;
+using System.Threading;
 class Search
 {
  
@@ -60,8 +62,7 @@ class Search
             }
             if (_BCresults == false)
             {  
-                Console.WriteLine("Loading...\n\n");
-                
+                Console.WriteLine("Loading...");
             }
        
         }
@@ -98,8 +99,9 @@ class Search
         
         if (_BCresults == false)
         {  
-            Console.WriteLine("Invalid input or scripture doesn't exist, please try again!\n\n");
-             _BCverse = "false";
+            Console.WriteLine("Invalid input or scripture doesn't exist, please try again!");
+            _BCverse = "false";
+            Thread.Sleep(2500);
         }
         return _BCverse;
     }
@@ -114,74 +116,102 @@ class Word
         List<string> passage = new List<string>(_BCarray);
         return passage;
     }
-
-    public List<string> BC_word_removal(List<string> _BCpassage)
+    public List<string> BC_word_removal(List<string> _BCpassage, int _BCword_drop)
     {
         Random random = new Random();
         int _BCrange = _BCpassage.Count; 
         bool _BCend = true;
         string _BCword;
         int _BCrandom_word;
+        List<int> _BCindexed_list = new List<int>();
         
-        do
-        {
-            _BCrandom_word = random.Next(0,_BCrange);
-            _BCword = _BCpassage[_BCrandom_word];
-            _BCend = true;
-
-            if (_BCword.Contains("_"))
+        for ( int i = 0; i < _BCword_drop; i++)
+           
+           { do
             {
-                _BCend = false;
-            }
+                _BCrandom_word = random.Next(0,_BCrange);
+                _BCword = _BCpassage[_BCrandom_word];
+                _BCend = true;
+
+                if (_BCword.Contains("_"))
+                {
+                    _BCindexed_list.Add(_BCrandom_word);
+                    _BCend = false;
+                    if (_BCindexed_list.Count() == _BCpassage.Count())
+                    {
+                        return _BCpassage;
+                    }
+                }
  
-        } while(_BCend != true);
-        
-    
-        
-        string _BCblank = "";
-        foreach (char letter in _BCword)  
-        {      
-            if (char.IsPunctuation(letter))
-            {
-                _BCblank += letter;
-            }
-           else
-            {
-                _BCblank += "_";
-            }
-        }
+            } while(_BCend != true);
 
-        _BCpassage[_BCrandom_word] = _BCblank;
+    
+
+            string _BCblank = "";
+            foreach (char letter in _BCword)  
+            {      
+                if (char.IsPunctuation(letter))
+                {
+                    _BCblank += letter;
+                }
+               else
+                {
+                    _BCblank += "_";
+                }
+            
+
+            _BCpassage[_BCrandom_word] = _BCblank;
+        }}
+        
         return _BCpassage;
     }
-
-    
-    
-    
-    
     public void BC_display(List<string> _BCpassage, string _BCscripture_location)
     {
         Console.Clear();
         
-        Console.WriteLine("Press enter to remove a word.");
+        Console.WriteLine("Enter \"end\" to leave.");
+        Console.WriteLine("Press enter to remove a word, and reveal to see the full scripture.\n");
         Console.WriteLine($"{_BCscripture_location}:");
         foreach (string word in _BCpassage)
         {
             Console.Write($"{word} ");
         }
+    }   
+    public string BC_reveal_display(string _BCverse, string _BCscripture_location)
+    {
+        Console.Clear();
+        Console.WriteLine("Enter \"end\" to leave.");
+        Console.WriteLine("Press enter to remove a word, and reveal to see the full scripture.\n");
+        Console.WriteLine($"{_BCscripture_location}:");
+        Console.Write(_BCverse + " ");
+        string _BCpause_code = Console.ReadLine();
+        return _BCpause_code;
     }
-
-    
-    
-    
     public int BC_get_word_drop_rate()
     {
-        Console.Write("How many words would you like to drop at a time: ");
-        string input = Console.ReadLine();
-        int _BCword_drop = int.Parse(input);
+        Console.Clear();
+        bool _BCboolVar = true;
+        int _BCword_drop = 1;
+
+        do
+        {
+            try
+            {
+                Console.Write("How many words would you like to drop at a time: ");
+                string input = Console.ReadLine();
+                _BCword_drop = int.Parse(input);
+                _BCboolVar = true;
+            }
+        
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input entered, please enter an integer.");
+                _BCboolVar = false;
+            }
+        } while (_BCboolVar != true);
+        
         return _BCword_drop;
     }
-
 
 }
 class Program
@@ -190,42 +220,128 @@ class Program
     {
         Search _BCsearch = new Search();
         Word _BCword = new Word();
+        Program _BCprogram = new Program();
         
-        
-        string _BCscripture_location = "";
-        string _BCverse = "";
+        string _BCscripture_location;
+        string _BCverse;
+        int _BCword_drop = 1; 
         bool _BCend = false;
-        
-        
+        bool _BCrepeat = false;
+
+        do
+        {
+            do
+               {
+                    Console.Clear();
+
+                   _BCscripture_location = _BCsearch.BC_get_scripture_cite(); 
+               
+                   _BCverse = _BCsearch.BC_get_verse(_BCscripture_location);
+
+                   
+                   if (_BCverse != "false")
+                   { 
+                       _BCend = true;
+                       _BCword_drop = _BCword.BC_get_word_drop_rate();
+                   }
+                   
+                   else
+                   {
+                       _BCend = false;
+                   }
+
+               } while(_BCend == false);
+
+               
+               _BCprogram.BC_run_word_removal(_BCword_drop, _BCscripture_location, _BCverse);
+               
+               _BCrepeat = _BCprogram.BC_get_repeat();
+
+        } while( _BCrepeat == true);
+
         Console.Clear();
-        
+        Console.WriteLine("Thank you for using scripture memorizer today!");
        
-       do
-        {
-            _BCscripture_location = _BCsearch.BC_get_scripture_cite(); 
-        
-            _BCverse = _BCsearch.BC_get_verse(_BCscripture_location);
-            
-            if (_BCverse != "false")
-            { 
-                _BCend = true;
-            }
-        } while(_BCend == false);
-        
-        
-        List<string> _BCpassage = new List<string>(_BCword.BC_word_parse(_BCverse));
-
-
-        int _BCrange = _BCpassage.Count();
-        for (int i = 0; i < _BCrange; i++)
-        {
-            _BCword.BC_display(_BCpassage, _BCscripture_location);
-            _BCword.BC_word_removal(_BCpassage);
-            Console.Write("");
-            string _BCpause_code = Console.ReadLine();
-        }
-
-       Console.Clear();
-       Console.WriteLine("Can you recite it all?");
     }
+
+    public bool BC_get_repeat()
+    {
+        bool _BCrepeat = false;
+        Console.Clear();
+        Console.WriteLine("Can you recite it all?");
+
+        List<string> _BCaccepted_responses = new List<string>{"Yes", "yes", "y", "Y", "No", "no", "N","n"};
+            bool _BCbool = false;
+            string _BCresponse = "";
+            do
+            {
+            try
+            {
+                Console.Write("Would you like to memorize another scripture? ");
+                _BCresponse = Console.ReadLine();
+                if (!_BCaccepted_responses.Contains(_BCresponse))
+                {
+                    throw new ArgumentException("Sorry that is an invalid input");
+                }
+                else
+                {
+                    _BCbool = false;
+                }
+            }
+            
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+                _BCbool = true;
+            }
+        } while (_BCbool == true);
+
+        if (_BCresponse == "Yes" || _BCresponse == "yes" || _BCresponse == "y" || _BCresponse == "Y")
+            
+            {
+                _BCrepeat = true;
+            }
+
+        return _BCrepeat;
+    }
+
+    public void BC_run_word_removal(int _BCword_drop, string _BCscripture_location, string _BCverse)
+    {
+        Word _BCword = new Word();
+        string _BCpause_code = "";
+
+        List<string> _BCpassage = new List<string>(_BCword.BC_word_parse(_BCverse));
+        int _BCrange = _BCpassage.Count();
+
+            for (int i = 0; i < _BCrange; i += _BCword_drop)
+            {
+    
+                if (_BCpause_code == "end")
+                {
+                    //finish running the loop//
+                }
+
+                else
+                {
+                    _BCword.BC_display(_BCpassage, _BCscripture_location);
+                    
+                    Console.Write("");
+                    _BCpause_code = Console.ReadLine();
+                }
+                
+
+                if (_BCpause_code == "reveal" || _BCpause_code == "Reveal")
+                {
+                   _BCpause_code = _BCword.BC_reveal_display(_BCverse, _BCscripture_location);
+                }
+
+                else
+                {
+                    _BCword.BC_word_removal(_BCpassage, _BCword_drop);
+                }
+            }
+    }
+
+
+
 }
